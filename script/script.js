433161cac5fd5a1814b1b5af9b80114221d760ef
anishghimire <anishghimire862@gmail.com>
2019-01-24 22:09:46 +0545

    moveToBottom function seperated from topControl

--- script/script.js
+++ script/script.js
@@ -1,7 +1,7 @@
 var playerObject = null;
 var animate;
 var obstacleObject = null;
-
+var points = 0;
 playerObject = document.getElementById("player");
 obstacleObject = document.getElementById("obstacle");
 
@@ -18,40 +18,45 @@ obstacleObject.style.left = "400px";
 function rightControl() {
   playerObject.style.left = parseInt(playerObject.style.left) + 10 + 'px';
 }   
+function moveToBottom(delay) {
+      if(playerObject.style.bottom > 0 + 'px') {
+        playerObject.style.bottom = parseInt(playerObject.style.bottom) + -10 + 'px';
+      }
+      if(parseInt(playerObject.style.bottom) == 0) { 
+        clearInterval(delay);
+      }
+  }
   
 function topControl() {
   playerObject.style.bottom = parseInt(playerObject.style.bottom) + 50 + 'px';
   playerObject.style.left = parseInt(playerObject.style.left) + 30 + 'px';
-  if(playerObject.style.bottom >= 50 + 'px') {
-    var delay = setInterval(moveToBottom,50);
-    function moveToBottom() {
-        playerObject.style.bottom = parseInt(playerObject.style.bottom) + -10 + 'px';
-        console.log(playerObject.style.bottom);
-        if(parseInt(playerObject.style.bottom) == 0) { 
-          clearInterval(delay);
-        }
-    }        
-  }
+  var delay = setInterval(
+		function() {
+    	moveToBottom(delay) 
+		},
+	50);
 }
  
+
 document.body.onkeydown = function(e) {
   if(e.keyCode == 32) {
     topControl();
   }
 }
 
-var startGame = function() {
-  animate = setInterval(animateObstacle, 10);
+function startGame() {
+  animate = setInterval(animateObstacle, 100);
 }
  
 function animateObstacle() {
   obstacle.style.left = parseInt(obstacle.style.left) + -1 + 'px';      
   
 	if(obstacle.style.left == 0 + 'px') {
+    points++;
     obstacle.style.left = 400 + 'px';
   }
   
-	obstacle.style.width = Math.floor(Math.random()*10) + 'px';
+	// obstacle.style.width = Math.floor(Math.random()*10) + 'px';
   
 	var playerPos = playerObject.style.left;
   var obstaclePos = obstacle.style.left;
@@ -60,21 +65,20 @@ function animateObstacle() {
 }
 
 var detectCollision = function(playerPos, obstaclePos) {
-  let player = playerPos;
-  let obstacle = obstaclePos;
-  // console.log(player +" "+ obstacle);
-  if(player == obstacle) {
-    alert("Game Over");
+  if(playerPos == obstaclePos) {
+    alert("Game Over!! You scored " + points + " points.");
     location.reload();
   }     
 }  
 
-/* function dynamicObstacle() {
+
+/*
+function dynamicObstacle() {
   var newObstacle = document.createElement("div");
   newObstacle.classList.add("dynamicobstacle");
   newObstacle.style.width = 10 + 'px';
   newObstacle.style.height = 50 + 'px';
-  newObstacle.style.
+  newObstacle.style.bottom = -40 + 'px';
   var currentDiv = document.getElementById("box");
   currentDiv.after(newObstacle);
 } */
